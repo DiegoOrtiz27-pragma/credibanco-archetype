81207b901a32d6301464dbaf8395c2acb2077860
DiegoOrtiz27 <emanuelortizlopez@gmail.com>
2025-06-09 16:57:14 -0500

    Adaptador dynamo

--- infrastructure/driven-adapter/dynamodb/src/main/java/com/credibanco/swarch/mapper/ILoanTraceMapper.java
+++ infrastructure/driven-adapter/dynamodb/src/main/java/com/credibanco/swarch/mapper/ILoanTraceMapper.java
@@ -1,7 +1,6 @@
 package com.credibanco.swarch.mapper;
 
 import com.credibanco.swarch.entity.LoanTraceData;
-import com.credibanco.swarch.model.Loan;
 import com.credibanco.swarch.model.LoanTrace;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
